1fff6e5b058b93e87597f9cb5541531a64c6fb2e
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-21 22:02:55 +0500

    Task2

---
+++ Tyuiu.TolmachevVV.Sprint3.Task2.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolmachevVV.Sprint3.Task2.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolmachevVV.Sprint3.Task2.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9993bb80-2201-474c-965a-f84051ce678d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.TolmachevVV.Sprint3.Task2.V23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TolmachevVV.Sprint3.Task2.V23.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint3.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService service1 = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Толмачев. В. В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #3                                                                *");
+            Console.WriteLine("*Задание #2                                                               *");
+            Console.WriteLine("*Вариант #11                                                              *");
+            Console.WriteLine("*Выполнил: Толмачев Виталий Владимирович | ПКТб 23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = 1,5                                                                   *");
+            double x = 1.5;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(service1.GetSumSeries(x, 1, 13));
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
